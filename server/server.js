72f71a2e12c27f3a0c95948b1df9ee73dc140395
lequo <lequockhanhkt03@gmail.com>
2024-06-12 17:03:10 +0700

    chore: Update CORS configuration in server.js for better security

--- server/server.js
+++ server/server.js
@@ -16,11 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN,
-	})
-);
+app.use(cors());
 
 mongoose
 	.connect(process.env.MONGODB_URI)
