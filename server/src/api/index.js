d6c604027a9a039eae3dfd5c2006aac268e62656
lequo <lequockhanhkt03@gmail.com>
2024-06-13 10:35:29 +0700

    chore: Refactor cron job to fetch weather data for each user and send email updates

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -94,4 +94,40 @@ router.get("/unsubscribe", async (req, res) => {
 	}
 });
 
+router.get("/cron", async (req, res) => {
+	try {
+		const users = await User.find();
+		await users.forEach(async (user) => {
+			const response = await fetch(
+				`https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=id:${user.city}&days=4`
+			);
+			const data = await response.json();
+			const { forecastday } = data.forecast;
+			const forecast = forecastday.map((day) => {
+				return {
+					date: day.date,
+					temperature: day.day.avgtemp_c,
+					wind: day.day.maxwind_kph,
+					humidity: day.day.avghumidity,
+					icon: day.day.condition.icon,
+				};
+			});
+			const dataFormatted = {
+				city: data.location.name,
+				temperature: data.current.temp_c,
+				condition: data.current.condition.text,
+				icon: data.current.condition.icon,
+				wind: data.current.wind_kph,
+				humidity: data.current.humidity,
+				date: data.current.last_updated,
+				forecast,
+			};
+			sendWeatherUpdate(user.email, dataFormatted);
+			res.json({ message: "Email sent" });
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 module.exports = router;
